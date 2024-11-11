7389ecf565a4644ccf03399af62398b3cdf9a0c6
Ganesh Patil <ganeshpatil67284@gmail.com>
2024-11-11 21:30:54 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fingerprint-recog',
+  webDir: 'www'
+};
+
+export default config;
